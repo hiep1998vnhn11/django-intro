a748ca9582366b5544023639201e04e0ddc998db
Hiệp Trần <hiep1998vnhn11@gmail.com>
2021-11-09 07:45:11 +0700

    init app

---
+++ api/models/user.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'users'
